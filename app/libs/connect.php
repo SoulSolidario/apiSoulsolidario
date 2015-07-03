642aff898d07d4676293ad61a20299cd4875b9c1
Jordan Pena <jordanpena1@gmail.com>
2015-07-03 03:07:21 -0300

    Organização das pastas e classes da aplicação

--- app/libs/connect.php
+++ app/libs/connect.php
@@ -6,7 +6,7 @@ function getConnection()
 {
     try{
         $db_username = "root";
-        $db_password = "";
+        $db_password = "JOR@dan12";
         $connection = new PDO("mysql:host=localhost;dbname=soulsolidario", $db_username, $db_password);
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
